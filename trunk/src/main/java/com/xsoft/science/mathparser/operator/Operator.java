6e43ebb4483328570290cfb798439e37aa3d438f
daniel.rguez.hdez@gmail.com <daniel.rguez.hdez@gmail.com@94c30dc6-c84f-1113-3e61-b5e972a8920e>
2013-03-10 08:56:51 +0000

    Apache License 2.0

--- trunk/src/main/java/com/xsoft/science/mathparser/operator/Operator.java
+++ trunk/src/main/java/com/xsoft/science/mathparser/operator/Operator.java
@@ -1,6 +1,21 @@
 package com.xsoft.science.mathparser.operator;
 
-import java.util.List;
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import com.xsoft.science.mathparser.tokenStructure.TokenStack;
 
@@ -12,9 +27,7 @@ public class Operator implements Comparable<Operator> {
 	private int neededArguments;
 	private OperatorBehaviour behaviour;
 
-	public Operator(String symbol, int precedence,
-			OperatorAssociativity associativity, int neededArguments,
-			OperatorBehaviour behaviour) {
+	public Operator(String symbol, int precedence, OperatorAssociativity associativity, int neededArguments, OperatorBehaviour behaviour) {
 		this.symbol = symbol;
 		this.precedence = precedence;
 		this.associativity = associativity;
@@ -35,13 +48,9 @@ public class Operator implements Comparable<Operator> {
 	}
 
 	public int compareTo(Operator o) {
-		if (this.precedence > o.precedence) {
-			return 1;
-		} else if (this.precedence < o.precedence) {
-			return -1;
-		} else {
-			return 0;
-		}
+		if (this.precedence > o.precedence) { return 1;	} 
+		else if (this.precedence < o.precedence) { return -1; }
+		else { return 0; }
 	}
 
 	public int getNeededArguments() {
--- trunk/src/main/java/com/xsoft/science/mathparser/operator/utils/OperatorUtils.java
+++ trunk/src/main/java/com/xsoft/science/mathparser/operator/utils/OperatorUtils.java
@@ -1,9 +1,25 @@
 package com.xsoft.science.mathparser.operator.utils;
 
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 import java.util.Arrays;
 
 import com.xsoft.science.mathparser.constants.OperatorConstants;
-import com.xsoft.science.mathparser.constants.OperatorConstantsFactory;
 import com.xsoft.science.mathparser.factories.Factories;
 import com.xsoft.science.mathparser.operator.Operator;
 import com.xsoft.science.mathparser.operator.OperatorAssociativity;
@@ -12,29 +28,24 @@ public final class OperatorUtils {
 
 	public static Operator getOperator(String operator) {
 		try {
-			int index = Arrays.asList(OperatorConstants.validOperatorsSymbols)
-					.indexOf(operator);
+			int index = Arrays.asList(OperatorConstants.validOperatorsSymbols).indexOf(operator);
 			return OperatorConstants.validOperators[index];
 		} catch (IndexOutOfBoundsException e) {
-			Factories.getExceptionFactoryInstance()
-					.launchInvalidOperatorException();
+			Factories.getExceptionFactoryInstance().launchInvalidOperatorException();
 			return null;
 		}
 	}
 
 	public static boolean isAnValidSymbol(String candidate) {
-		return Arrays.asList(OperatorConstants.validOperatorsSymbols).indexOf(
-				candidate) != -1;
+		return Arrays.asList(OperatorConstants.validOperatorsSymbols).indexOf(candidate) != -1;
 	}
 
 	public static boolean isAParentheses(String candidate) {
-		return Arrays.asList(OperatorConstants.parenthesesSymbols).indexOf(
-				candidate) != -1;
+		return Arrays.asList(OperatorConstants.parenthesesSymbols).indexOf(candidate) != -1;
 	}
 
 	public static boolean isAnOperator(String candidate) {
-		return Arrays.asList(OperatorConstants.validOperatorsSymbols).indexOf(
-				candidate) != -1;
+		return Arrays.asList(OperatorConstants.validOperatorsSymbols).indexOf(candidate) != -1;
 	}
 
 	public static boolean isLeftAssociativity(String operator) {
@@ -51,7 +62,7 @@ public final class OperatorUtils {
 
 		return operatorObj1.compareTo(operatorObj2);
 	}
-	
+
 	public static boolean isNumber(String candidate) {
 		try {
 			Integer.parseInt(candidate);
