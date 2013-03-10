813915cf462f90ad7594be0c2c5407b8ba85eedf
daniel.rguez.hdez <daniel.rguez.hdez@gmail.com>
2013-03-10 09:02:10 +0000

    Apache License 2.0

--- src/main/java/com/xsoft/science/mathparser/parser/calculation/impl/RPNCalculatorImpl.java
+++ src/main/java/com/xsoft/science/mathparser/parser/calculation/impl/RPNCalculatorImpl.java
@@ -1,6 +1,21 @@
 package com.xsoft.science.mathparser.parser.calculation.impl;
 
-import java.util.List;
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import com.xsoft.science.mathparser.constants.utils.MathParserUtils;
 import com.xsoft.science.mathparser.factories.Factories;
@@ -16,10 +31,8 @@ import com.xsoft.science.mathparser.tokenStructure.TokenStructureFactory;
 
 public class RPNCalculatorImpl implements RPNCalculator {
 
-	private final CalculatorFactory calculatorFactory = Factories
-			.getCalculatorFactoryInstance();
-	private final ExceptionFactory exceptionFactory = Factories
-			.getExceptionFactoryInstance();
+	private final CalculatorFactory calculatorFactory = Factories.getCalculatorFactoryInstance();
+	private final ExceptionFactory exceptionFactory = Factories.getExceptionFactoryInstance();
 
 	private TokenQueue expressionInTokens;
 	private TokenStack rpnStack;
@@ -27,7 +40,6 @@ public class RPNCalculatorImpl implements RPNCalculator {
 	private String actualToken;
 	private Operator actualOperator;
 	private int pointer;
-	private List<Double> arguments;
 
 	public void setRPNExpression(TokenQueue expression) {
 		initialize();
