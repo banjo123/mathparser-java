6843928fce3b435a52527cb83a50628b8188c050
daniel.rguez.hdez <daniel.rguez.hdez@gmail.com>
2013-03-10 14:37:16 +0000

    [MathParser] Package rename

---
+++ src/main/java/com/google/code/mathparser/factories/exception/messages/ExceptionMessageFactory.java
@@ -0,0 +1,35 @@
+package com.google.code.mathparser.factories.exception.messages;
+
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+ daniel.rguez.hdez[at]gmail.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+public interface ExceptionMessageFactory {
+	
+	public String createInsufficientParametersMessage(String operator, int numberExpectedArguments, int numberFoundArguments);
+
+	public String createInvalidCharacterMessage(String invalidCharacter);
+
+	public String createInvalidExpressionMessage();
+
+	public String createInvalidOperatorMessage();
+
+	public String createMismatchedParenthesesMessage();
+
+	public String createTooManyValuesMessage();
+
+}
---
+++ src/main/java/com/google/code/mathparser/tokenStructure/TokenQueueImpl.java
@@ -0,0 +1,75 @@
+package com.google.code.mathparser.tokenStructure;
+
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+ daniel.rguez.hdez[at]gmail.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenQueueImpl implements TokenQueue {
+	private List<String> tokens;
+	private StringBuffer expressionString;
+
+	public TokenQueueImpl() {
+		tokens = new ArrayList<String>();
+		expressionString = new StringBuffer();
+	}
+
+	public List<String> getTokens() {
+		return tokens;
+	}
+
+	public void addToken(String token) {
+		tokens.add(token);
+		expressionString.append(token);
+	}
+
+	public int size() {
+		return tokens.size();
+	}
+
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("[");
+		for (int i = 0; i < tokens.size(); i++) {
+			if (i == tokens.size() - 1) { buf.append(" ".concat(tokens.get(i)));} 
+			else { buf.append(" ".concat(tokens.get(i).concat(";"))); }
+		}
+		buf.append("]");
+		return buf.toString();
+	}
+
+	public String getTokenAt(int pointer) {
+		return tokens.get(pointer);
+	}
+
+	public boolean hasMoreTokens() {
+		return tokens.size() > 0;
+	}
+
+	public String toExpressionString() {
+		return this.expressionString.toString();
+	}
+
+	public String getLastToken() {
+		if (hasMoreTokens()) {
+			return tokens.get(tokens.size() - 1);
+		}
+		return null;
+	}
+}
