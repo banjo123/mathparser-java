0f1ad5bd60ebbd4464b8cb6c2747bca4ab0831af
daniel.rguez.hdez@gmail.com <daniel.rguez.hdez@gmail.com@94c30dc6-c84f-1113-3e61-b5e972a8920e>
2013-03-10 08:53:29 +0000

    Apache License 2.0

--- trunk/src/main/java/com/xsoft/science/mathparser/operator/behaviour/AddOperatorBehaviour.java
+++ trunk/src/main/java/com/xsoft/science/mathparser/operator/behaviour/AddOperatorBehaviour.java
@@ -1,5 +1,22 @@
 package com.xsoft.science.mathparser.operator.behaviour;
 
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 import com.xsoft.science.mathparser.factories.Factories;
 import com.xsoft.science.mathparser.operator.OperatorBehaviour;
 import com.xsoft.science.mathparser.tokenStructure.TokenStack;
@@ -32,15 +49,13 @@ public class AddOperatorBehaviour implements OperatorBehaviour {
 		}
 
 		if (hasSufficientArguments(invertedStack)) {
-			argument1 = Double.parseDouble((invertedStack.size() > 0) ? invertedStack.pop()	: "0.0");
-			argument2 = Double.parseDouble((invertedStack.size() > 0) ? invertedStack.pop()	: "0.0");
+			argument1 = Double.parseDouble((invertedStack.size() > 0) ? invertedStack.pop() : "0.0");
+			argument2 = Double.parseDouble((invertedStack.size() > 0) ? invertedStack.pop() : "0.0");
 		} else {
-			Factories.getExceptionFactoryInstance()
-			.launchInsufficientParamtersException(symbol,
-					neededArguments, invertedStack.size());
+			Factories.getExceptionFactoryInstance().launchInsufficientParamtersException(symbol, neededArguments, invertedStack.size());
 		}
 	}
-	
+
 	private boolean hasSufficientArguments(TokenStack argumentsStack) {
 		return argumentsStack.size() >= 1;
 	}
