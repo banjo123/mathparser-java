5ebe7da3ff2633efdb566cd3769ff7876569fd94
daniel.rguez.hdez <daniel.rguez.hdez@gmail.com>
2013-03-10 08:56:51 +0000

    Apache License 2.0

--- src/main/java/com/xsoft/science/mathparser/operator/OperatorBehaviourFactory.java
+++ src/main/java/com/xsoft/science/mathparser/operator/OperatorBehaviourFactory.java
@@ -1,5 +1,22 @@
 package com.xsoft.science.mathparser.operator;
 
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 import com.xsoft.science.mathparser.operator.behaviour.AddOperatorBehaviour;
 import com.xsoft.science.mathparser.operator.behaviour.DivOperatorBehaviour;
 import com.xsoft.science.mathparser.operator.behaviour.MulOperatorBehaviour;
@@ -9,28 +26,23 @@ import com.xsoft.science.mathparser.operator.behaviour.SubsOperatorBehaviour;
 
 public class OperatorBehaviourFactory {
 
-	public static OperatorBehaviour createAddBehaviour(String symbol,
-			int neededArguments) {
+	public static OperatorBehaviour createAddBehaviour(String symbol, int neededArguments) {
 		return new AddOperatorBehaviour(symbol, neededArguments);
 	}
 
-	public static OperatorBehaviour createSubsBehaviour(String symbol,
-			int neededArguments) {
+	public static OperatorBehaviour createSubsBehaviour(String symbol, int neededArguments) {
 		return new SubsOperatorBehaviour(symbol, neededArguments);
 	}
 
-	public static OperatorBehaviour createMulBehaviour(String symbol,
-			int neededArguments) {
+	public static OperatorBehaviour createMulBehaviour(String symbol, int neededArguments) {
 		return new MulOperatorBehaviour(symbol, neededArguments);
 	}
 
-	public static OperatorBehaviour createDivBehaviour(String symbol,
-			int neededArguments) {
+	public static OperatorBehaviour createDivBehaviour(String symbol, int neededArguments) {
 		return new DivOperatorBehaviour(symbol, neededArguments);
 	}
 
-	public static OperatorBehaviour createPowBehaviour(String symbol,
-			int neededArguments) {
+	public static OperatorBehaviour createPowBehaviour(String symbol, int neededArguments) {
 		return new PowOperatorBehaviour(symbol, neededArguments);
 	}
 
--- src/main/java/com/xsoft/science/mathparser/operator/OperatorFactory.java
+++ src/main/java/com/xsoft/science/mathparser/operator/OperatorFactory.java
@@ -1,83 +1,78 @@
 package com.xsoft.science.mathparser.operator;
 
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 public class OperatorFactory {
 
 	private static String symbol;
 	private static int neededArguments;
 	private static int precedence;
 
-	private static Operator createOperator(String symbol, int precedence,
-			OperatorAssociativity associativity, int neededArguments,
-			OperatorBehaviour operatorBehaviour) {
-		return new Operator(symbol, precedence, associativity, neededArguments,
-				operatorBehaviour);
+	private static Operator createOperator(String symbol, int precedence, OperatorAssociativity associativity, int neededArguments,	OperatorBehaviour operatorBehaviour) {
+		return new Operator(symbol, precedence, associativity, neededArguments,	operatorBehaviour);
 	}
 
 	public static Operator createAddOperator() {
 		symbol = "+";
 		neededArguments = 2;
 		precedence = 2;
-		return OperatorFactory.createOperator(symbol, precedence,
-				OperatorAssociativity.LEFT, neededArguments,
-				OperatorBehaviourFactory.createAddBehaviour(symbol,
-						neededArguments));
+		return OperatorFactory.createOperator(symbol, precedence, OperatorAssociativity.LEFT, neededArguments, OperatorBehaviourFactory.createAddBehaviour(symbol, neededArguments));
 	}
 
 	public static Operator createSubsOperator() {
 		symbol = "-";
 		neededArguments = 2;
 		precedence = 2;
-		return OperatorFactory.createOperator(symbol, precedence,
-				OperatorAssociativity.LEFT, neededArguments,
-				OperatorBehaviourFactory.createSubsBehaviour(symbol,
-						neededArguments));
+		return OperatorFactory.createOperator(symbol, precedence, OperatorAssociativity.LEFT, neededArguments, OperatorBehaviourFactory.createSubsBehaviour(symbol,	neededArguments));
 	}
 
 	public static Operator createMulOperator() {
 		symbol = "*";
 		neededArguments = 2;
 		precedence = 3;
-		return OperatorFactory.createOperator(symbol, precedence,
-				OperatorAssociativity.LEFT, neededArguments,
-				OperatorBehaviourFactory.createMulBehaviour(symbol,
-						neededArguments));
+		return OperatorFactory.createOperator(symbol, precedence, OperatorAssociativity.LEFT, neededArguments, OperatorBehaviourFactory.createMulBehaviour(symbol, neededArguments));
 	}
 
 	public static Operator createDivOperator() {
 		symbol = "/";
 		neededArguments = 2;
 		precedence = 3;
-		return OperatorFactory.createOperator(symbol, precedence,
-				OperatorAssociativity.LEFT, neededArguments,
-				OperatorBehaviourFactory.createDivBehaviour(symbol,
-						neededArguments));
+		return OperatorFactory.createOperator(symbol, precedence, OperatorAssociativity.LEFT, neededArguments, OperatorBehaviourFactory.createDivBehaviour(symbol, neededArguments));
 	}
 
 	public static Operator createPowOperator() {
 		symbol = "^";
 		neededArguments = 2;
 		precedence = 4;
-		return OperatorFactory.createOperator(symbol, precedence,
-				OperatorAssociativity.RIGHT, neededArguments,
-				OperatorBehaviourFactory.createPowBehaviour(symbol,
-						neededArguments));
+		return OperatorFactory.createOperator(symbol, precedence, OperatorAssociativity.RIGHT, neededArguments, OperatorBehaviourFactory.createPowBehaviour(symbol, neededArguments));
 	}
 
 	public static Operator createLeftParentheses() {
 		symbol = "(";
 		neededArguments = 0;
 		precedence = 1;
-		return OperatorFactory.createOperator(symbol, precedence,
-				OperatorAssociativity.LEFT, neededArguments,
-				OperatorBehaviourFactory.createNoBehaviour());
+		return OperatorFactory.createOperator(symbol, precedence, OperatorAssociativity.LEFT, neededArguments, OperatorBehaviourFactory.createNoBehaviour());
 	}
 
 	public static Operator createRightParentheses() {
 		symbol = ")";
 		neededArguments = 0;
 		precedence = 1;
-		return OperatorFactory.createOperator(symbol, neededArguments,
-				OperatorAssociativity.LEFT, precedence,
-				OperatorBehaviourFactory.createNoBehaviour());
+		return OperatorFactory.createOperator(symbol, neededArguments, OperatorAssociativity.LEFT, precedence, OperatorBehaviourFactory.createNoBehaviour());
 	}
 }
