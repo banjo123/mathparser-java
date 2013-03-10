b9c765ac24dd6b16edbfbdac954026494047f40b
daniel.rguez.hdez@gmail.com <daniel.rguez.hdez@gmail.com@94c30dc6-c84f-1113-3e61-b5e972a8920e>
2013-03-10 08:48:33 +0000

    Apache License 2.0

--- trunk/src/main/java/com/xsoft/science/mathparser/factories/exception/messages/impl/ExceptionMessageFactoryImpl.java
+++ trunk/src/main/java/com/xsoft/science/mathparser/factories/exception/messages/impl/ExceptionMessageFactoryImpl.java
@@ -1,18 +1,32 @@
 package com.xsoft.science.mathparser.factories.exception.messages.impl;
 
+/*
+ MathParser Java - Cross Platform Mathematical Expressions Parser
+ Copyright 2013 Rodríguez Hernández, Daniel.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 import com.xsoft.science.mathparser.constants.ErrorMessage;
 import com.xsoft.science.mathparser.factories.exception.messages.ExceptionMessageFactory;
 
 public class ExceptionMessageFactoryImpl implements ExceptionMessageFactory {
 
-	public String createInsufficientParametersMessage(String operator,
-			int numberExpectedArguments, int numberFoundArguments) {
+	public String createInsufficientParametersMessage(String operator, int numberExpectedArguments, int numberFoundArguments) {
 		String message = ErrorMessage.INSUFFICIENT_PARAMETERS;
 		message = message.replace("%OPERATOR%", operator);
-		message = message.replace("%EXPECTED%",
-				String.valueOf(numberExpectedArguments));
-		message = message.replace("%FOUND%",
-				String.valueOf(numberFoundArguments));
+		message = message.replace("%EXPECTED%",	String.valueOf(numberExpectedArguments));
+		message = message.replace("%FOUND%", String.valueOf(numberFoundArguments));
 		return message;
 	}
 
